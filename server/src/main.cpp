6f1dd6956a925c0802bb23335182a82e98333e9d
pdaejun <pdaejun@gmail.com>
2014-12-04 21:09:59 -0800

    sensor problem solved

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -7,14 +7,19 @@
 #define PRT_ERR(fmt, arg...) \
 	do { SLOG(LOG_ERR, LOG_TAG, fmt, ##arg); } while (0)
 
-Ecore_Timer         *timer     = NULL;
+Ecore_Timer         *sensor_timer     = NULL;
+Ecore_Timer         *redraw_timer     = NULL;
 Ecore_Event_Handler *handler   = NULL;
 SerialReader serialReader;
 int temp=0, hum=0;
-
-
 Eina_Bool
-getSensor_redrawScreen(void *data)
+getSensor(void *data)
+{
+	serialReader.listen();
+	return ECORE_CALLBACK_RENEW;
+}
+Eina_Bool
+redrawScreen(void *data)
 {
    temp = serialReader.get_Temperature();
    hum = serialReader.get_Humidity();
@@ -28,9 +33,6 @@ exit_func(void *data, int ev_type, void *ev)
    Ecore_Event_Signal_Exit *e;
 
    e = (Ecore_Event_Signal_Exit *)ev;
-   if (e->interrupt)      printf("Exit: interrupt\n");
-   else if (e->quit)      printf("Exit: quit\n");
-   else if (e->terminate) printf("Exit: terminate\n");
    ecore_main_loop_quit();
    return 1;
 }
@@ -40,10 +42,9 @@ main(int argc, const char **argv)
 {
    ecore_init();
    ecore_app_args_set(argc, argv);
-
-
    handler = ecore_event_handler_add(ECORE_EVENT_SIGNAL_EXIT, exit_func, NULL);
-   timer = ecore_timer_add(0.5, getSensor_redrawScreen, NULL);
+   sensor_timer = ecore_timer_add(1, getSensor, NULL);
+   redraw_timer = ecore_timer_add(1.5, redrawScreen, NULL);
    ecore_main_loop_begin();
    ecore_shutdown();
    return 0;
